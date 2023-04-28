ab92d59d57528aa15227d6af39daf55360d4e5fb
nazarkhanov <joni.mypth@gmail.com>
2023-04-28 23:54:36 +0600

    fix: dump

--- cabinet/dump/view.py
+++ cabinet/dump/view.py
@@ -1,11 +1,12 @@
 import os
 import json
 import subprocess
+from django.conf import settings
 from django.http import HttpResponse
 
 
 def dump_request(request):
-    sql_dump = subprocess.check_output(['python', 'manage.py', 'dumpdata'])
+    sql_dump = subprocess.check_output(['python', os.path.join(settings.BASE_DIR, 'manage.py'), 'dumpdata'])
     sql_dump = sql_dump.decode("utf-8")
     sql_dump = json.loads(sql_dump)
 
